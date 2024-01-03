8283978b1c88988e8e09a47bf249b4c75643398d
phout818 <phztro@gmail.com>
2024-01-03 21:54:17 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Head } from "./componet/head";
 function App() {
   return (
     <div className="App">
+      <Head></Head>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Learn React haha xd</p>
       </header>
     </div>
   );
---
+++ src/componet/head.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export function Head(props) {
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "darksalmon",
+          width: "100%",
+          height: 30,
+          marginTop: 5,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <b style={{ color: "darkblue" }}>Book Shop</b>
+      </div>
+    </>
+  );
+}
